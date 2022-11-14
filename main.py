71ba89c5b813ef5979d047f69466bfe098f68389
ryanoconnor <ryan19.roc@gmail.com>
2022-11-14 21:26:25 +0000

    Analyse the dataset showing the station id and names. Add function that will allow for all historical data from a specific weather station to be obtained by the user.

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def home():
     return render_template("home.html",data=station_table.to_html())
 
 
+# Function to allow the users to input a specific year and date to retrieve the data
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
     # Concatenate the user input station id to the file route
@@ -25,6 +26,19 @@ def about(station, date):
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+# Function to display all data for a station
+@app.route("/api/v1/<station>")
+def all_data(station):
+    # Concatenate the user input station id to the file route
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+
+    # Read the dataframe
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+
+    # Turn the data to a dictionary
+    data = df.to_dict(orient="records")
+    return data
 
 
 app.run(debug=True)
